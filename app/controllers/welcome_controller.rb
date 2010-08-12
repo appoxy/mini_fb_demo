cbe32032e2f03a2ea41df0731cbd54a72da89e70
Travis Reeder <travis@appoxy.com>
2010-08-11 17:04:11 -0700

    Added posting form.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,16 +6,21 @@ class WelcomeController < ApplicationController
         @oauth_url = MiniFB.oauth_url(FB_APP_ID, HOST + "sessions/create",
                                       :scope=>MiniFB.scopes.join(","))
 
+        @post_tos = ["me"]
+        @post_types = ["feed"]
+
     end
 
     def show
+        MiniFB.enable_logging
         @id = params[:id]
         @res = MiniFB.get(@access_token, @id, :type=>params[:type], :metadata=>true)
     end
 
     def post
-        @id = params[:id]
-        @res = MiniFB.post(@access_token, @id, :type=>params[:type], :metadata=>true)
+        @id = params.delete :id
+        @to = params.delete :to
+        @res = MiniFB.post(@access_token, @id, :type=>params[:type], :metadata=>true, :params=>params)
         redirect_to :action=>"show", :id=>@id
     end
 
